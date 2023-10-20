d91d30596d7a091430b264a797350140dd7cc980
gallone10 <enzo.gallone83@gmail.com>
2023-10-20 09:55:52 -0300

    css

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,10 +4,10 @@ export default function Cabecalho() {
   return (
     <header className='bg-gray-700 text-white h-20'>
     <nav className="flex flex-row items-center justify-around">
-      <Link href="/">HOME</Link>
-      <Link href="/produtos/mamao">MAMÃO</Link>
-      <Link href="/produtos/melao">MELÃO</Link>
-      <Link href="/produtos/manga">MANGA</Link>
+      <Link href="/" className="block py-6 hover:bg-black font-extrabold">HOME</Link>
+      <Link href="/" className="block py-6 hover:bg-black font-extrabold">MAMÃO</Link>
+      <Link href="/" className="block py-6 hover:bg-black font-extrabold">MELÃO</Link>
+      <Link href="/" className="block py-6 hover:bg-black font-extrabold">MANGA</Link>
     </nav>
   </header>
   )
